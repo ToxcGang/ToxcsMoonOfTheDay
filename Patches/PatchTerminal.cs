bbcbe2bfd1009ca55480c913d14cc9e287d6015a
Toxc <125293391+ToxcGang@users.noreply.github.com>
2026-04-28 22:09:29 -0600

    Refactor PatchTerminal.cs for improved functionality

--- Patches/PatchTerminal.cs
+++ Patches/PatchTerminal.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Linq;
 using HarmonyLib;
 using UnityEngine;
 
-namespace MoonOfTheDay.Patches;
+namespace ToxcsMoonOfTheDay.Patches;
 
 internal static class PatchTerminalHelpers
 {
@@ -11,60 +11,47 @@ internal static class PatchTerminalHelpers
     {
         Plugin.Logger.LogDebug($"Adding terminal command for {moonToAdd.PlanetName}...");
 
-        // Find the Confirm, Deny & Route TerminalKeywords
         var confirmKeyword = terminal.terminalNodes.allKeywords.First(keyword => keyword.name == "Confirm");
         var denyKeyword = terminal.terminalNodes.allKeywords.First(keyword => keyword.name == "Deny");
         var routeKeyword = terminal.terminalNodes.allKeywords.First(keyword => keyword.name == "Route");
 
-        // Find the CancelRoute TerminalNode
-        // var cancelRouteNode = Object.FindObjectsOfType<TerminalNode>().First(node => node.name == "CancelRoute");    -- This doesn't work for some reason, but would be less prone to breaking on updates
-        var cancelRouteNode = routeKeyword.compatibleNouns[0].result.terminalOptions[0].result;
+        var cancelRouteNode = routeKeyword.compatibleNouns.First().result.terminalOptions.First(opt => opt.noun == denyKeyword).result;
 
-        // Create keyword
         var moonKeyword = ScriptableObject.CreateInstance<TerminalKeyword>();
         moonKeyword.name = GetKWNameForMoon(moonToAdd);
         moonKeyword.word = GetKWWordforMoon(moonToAdd);
         moonKeyword.defaultVerb = routeKeyword;
         moonKeyword.compatibleNouns = Array.Empty<CompatibleNoun>();
 
-        // Create travel node
         var travelNode = ScriptableObject.CreateInstance<TerminalNode>();
         travelNode.name = GetTravelNodeNameForMoon(moonToAdd);
-        travelNode.displayText =
-            "Routing autopilot to " + moonToAdd.PlanetName + ".\n\nPlease enjoy your flight.";
+        travelNode.displayText = "Routing autopilot to " + moonToAdd.PlanetName + ".\n\nPlease enjoy your flight.";
         travelNode.clearPreviousText = true;
         travelNode.buyRerouteToMoon = moonToAdd.levelID;
 
-        // Create TerminalNode for decision to travel to moon
         var travelDecisionNode = ScriptableObject.CreateInstance<TerminalNode>();
         travelDecisionNode.name = GetKWNameForMoon(moonToAdd);
-        travelDecisionNode.displayText =
-            "The company has detected a rogue planet. It might not be available for long. Do you want to go there?\n\nIt is currently [currentPlanetTime] on this moon.\n\nPlease CONFIRM or DENY.\n\n";
+        travelDecisionNode.displayText = "The company has detected a rogue planet. It might not be available for long. Do you want to go there?\n\nIt is currently [currentPlanetTime] on this moon.\n\nPlease CONFIRM or DENY.\n\n";
         travelDecisionNode.clearPreviousText = true;
         travelDecisionNode.displayPlanetInfo = moonToAdd.levelID;
-        travelDecisionNode.buyRerouteToMoon =
-            -2; // Based on UnityExplorer values. Not sure if this *needs* to be -2. OnSubmit() seems to handle -2 differently.
+        travelDecisionNode.buyRerouteToMoon = -2;
         travelDecisionNode.overrideOptions = true;
         travelDecisionNode.terminalOptions = new[]
         {
-            new CompatibleNoun { noun = denyKeyword, result = cancelRouteNode },
-            new CompatibleNoun { noun = confirmKeyword, result = travelNode }
+            new CompatibleNoun(denyKeyword, cancelRouteNode),
+            new CompatibleNoun(confirmKeyword, travelNode)
         };
 
-        // Add the TerminalKeyword to the Terminal
         var allKeywords = terminal.terminalNodes.allKeywords.ToList();
         allKeywords.Add(moonKeyword);
         terminal.terminalNodes.allKeywords = allKeywords.ToArray();
 
-        // Expand route keyword to include the moon as compatible noun
         var compatibleNouns = routeKeyword.compatibleNouns.ToList();
-        compatibleNouns.Add(new CompatibleNoun { noun = moonKeyword, result = travelDecisionNode });
+        compatibleNouns.Add(new CompatibleNoun(moonKeyword, travelDecisionNode));
         routeKeyword.compatibleNouns = compatibleNouns.ToArray();
 
-        // Add the moon name to the Moons TerminalKeyword
         terminal.terminalNodes.allKeywords.First(keyword => keyword.name == "Moons").specialKeywordResult
-                .displayText +=
-            "* " + moonToAdd.PlanetName + " [planetTime]\n";
+                .displayText += "* " + moonToAdd.PlanetName + " [planetTime]\n";
 
         Plugin.Logger.LogDebug($"Added terminal command for {moonToAdd.PlanetName}.");
     }
@@ -94,27 +81,15 @@ public class PatchTerminal
     {
         Plugin.Logger.LogDebug("Inserting moons in Terminal...");
 
-        var dailyMoon = Plugin.GetDailyMoon(__instance.moonsCatalogueList);
-        var weeklyMoon = Plugin.GetWeeklyMoon(__instance.moonsCatalogueList);
-
-        var moonList = __instance.moonsCatalogueList.ToList();
-
-        // Remove first to prevent potential issues with duplicate moons -- same ID should prevent issues with doing this
-        moonList.RemoveAll(moon => moon.PlanetName == dailyMoon.PlanetName);
-        moonList.RemoveAll(moon => moon.PlanetName == weeklyMoon.PlanetName);
-
-        moonList.Add(dailyMoon);
-        moonList.Add(weeklyMoon);
-
-        __instance.moonsCatalogueList = moonList.ToArray();
+        __instance.moonsCatalogueList = Plugin.WithCustomMoons(__instance.moonsCatalogueList);
     }
 
     [HarmonyPatch("Awake")]
     [HarmonyPostfix]
     public static void AddTerminalCommands(Terminal __instance)
     {
-        var dailyMoon = Plugin.GetDailyMoon(__instance.moonsCatalogueList);
-        var weeklyMoon = Plugin.GetWeeklyMoon(__instance.moonsCatalogueList);
+        var dailyMoon = __instance.moonsCatalogueList.First(moon => moon.PlanetName == Plugin.DailyMoonName);
+        var weeklyMoon = __instance.moonsCatalogueList.First(moon => moon.PlanetName == Plugin.WeeklyMoonName);
 
         if (__instance.terminalNodes.allKeywords.Any(keyword =>
                 keyword.name == PatchTerminalHelpers.GetKWNameForMoon(dailyMoon)))
@@ -128,7 +103,6 @@ public class PatchTerminal
         PatchTerminalHelpers.AddTerminalCommand(ref __instance, dailyMoon);
         PatchTerminalHelpers.AddTerminalCommand(ref __instance, weeklyMoon);
 
-        // Add extra newline for spacing
         __instance.terminalNodes.allKeywords.First(keyword => keyword.name == "Moons").specialKeywordResult
             .displayText += "\n";
     }
